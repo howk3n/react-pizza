afd5a740dcb708aedc16ad0b4df6804716442d73
eriukiyo <dusancvijic1906@gmail.com>
2024-06-29 23:58:05 +0200

    display pizzas in menu

--- src/features/menu/Menu.jsx
+++ src/features/menu/Menu.jsx
@@ -1,16 +1,22 @@
 import { getMenu } from "@/services/apiRestaurant";
 import { useLoaderData } from "react-router-dom";
-
-function Menu() {
-  const menu = useLoaderData();
-  console.log(menu);
-
-  return <h1>Menu</h1>;
-}
+import MenuItem from "./MenuItem";
 
 export async function menuLoader() {
   const menu = await getMenu();
   return menu;
+}
+
+function Menu() {
+  const menu = useLoaderData();
+
+  return (
+    <ul>
+      {menu.map((data) => (
+        <MenuItem pizza={data} key={data.id} />
+      ))}
+    </ul>
+  );
 }
 
 export default Menu;
